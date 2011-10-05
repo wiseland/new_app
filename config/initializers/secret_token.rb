fcd784a143479b93eaabed7e16c7112486e51764
Wiseland <fsslapal@gmail.com>
2011-10-05 13:05:39 -0400

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp::Application.config.secret_token = '4d40f2e796ad74af04efd71c2b96f6d0384d8237bb8c5358acbdfa4089ee786065b07f136d238d1f69494221246adde3e6833945b47d29e05a47f9889ea8a6fd'
